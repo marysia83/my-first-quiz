29f9da937600729cbf6a49f8336033aad59217a5
Maria Galent <mariagalent@gmail.com>
2019-11-22 00:30:42 -0600

    Finished disable function for the save/submit button in scores.js

--- JS/scores.js
+++ JS/scores.js
@@ -1,1 +1,11 @@
-saveHighScore
+var username = document.getElementById("username");
+var saveScoreBtn = document.getElementById("saveScore");
+
+    username.addEventListener("keyup", function() {
+    console.log(username.value);
+    saveScoreBtn.disabled = !username.value; //if there is nothing entered, set the button to disabled
+});
+
+function saveHighScore(e) {
+    e.preventDefault(); //prevents the page from refreshing
+}
